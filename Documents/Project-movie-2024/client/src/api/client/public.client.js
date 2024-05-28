c1eb54c89f9ed9f954b7d6da311582b58ec30574
DucTran1412 <147167679+DucTran1412@users.noreply.github.com>
2024-05-28 07:43:24 +0700

    Update public.client.js

--- Documents/Project-movie-2024/client/src/api/client/public.client.js
+++ Documents/Project-movie-2024/client/src/api/client/public.client.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+const baseURL = "https://moonflix-api.vercel.app/api/v1/";
 
-const baseURL = "https://project-web-api.vercel.app/api/v1/";
+// const baseURL = "https://project-web-api.vercel.app/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
